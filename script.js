a0bc80c05737f725e244f858fbb3694e45055454
Moldovan Alex <alexmoldovan2009@gmail.com>
2025-12-18 22:43:26 +0200

    Final challenge

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener(('DOMContentLoaded'),()=>{
+    let email = document.querySelector('.form-control');
+    let paragrafError = document.querySelector('.errorMessage');
+    let form = document.querySelector('form');
+    paragrafError.innerHTML='';
+    let emailRegex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    form.addEventListener(('submit'),(e)=>{
+        e.preventDefault();
+        if(email.value.trim()==='')
+        {
+            paragrafError.innerHTML = 'Required value!';
+        }
+        else if(email.value.includes('example@mail.com') || email.value.length <=5)
+        {
+            paragrafError.innerHTML = 'Invalid mail!';
+        }
+        else if(emailRegex.test(email.value)===false)
+        {
+            paragrafError.innerHTML ='False mail, not respecting a valid one!';
+        }
+        else{
+            form.reset();
+            window.alert('Mail sent succesfully!');
+            paragrafError.innerHTML='';
+        }
+
+    })
+})
